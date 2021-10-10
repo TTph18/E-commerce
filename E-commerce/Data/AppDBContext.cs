fb2ef7a67d00502bae71e49dfa46a6c35027c07e
TranThiHaPhuong <59615434+TTph18@users.noreply.github.com>
2021-10-11 05:26:25 +0700

    Initial Commit

---
+++ E-commerce/Data/AppDBContext.cs
@@ -0,0 +1,19 @@
+﻿using E_commerce.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace E_commerce.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Products> Products { get; set; }
+    }
+}
---
+++ E-commerce/Data/Models/Products.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_commerce.Data.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+        public string PictureUrl { get; set; }
+    }
+}
